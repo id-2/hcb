c031374ae94a5850dbdd6e68474517bf94506733
Gary Tou <gary@garytou.com>
2022-01-02 04:05:22 -0800

    rubocop

--- app/helpers/react_helper.rb
+++ app/helpers/react_helper.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # https://github.com/reactjs/react-rails/pull/166#issuecomment-86178980
 # This allows a react component to render an erb block as children
 module ReactHelper
