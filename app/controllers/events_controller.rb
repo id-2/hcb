6c0db016fe27dffeaed1883e810f01dd6ad1093b
yodalightsabr <yoda@yodacode.xyz>
2022-10-06 10:16:22 -0700

    Remove whitespace

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -58,7 +58,7 @@ class EventsController < ApplicationController
     @pending_transactions = _show_pending_transactions
     @transactions = Kaminari.paginate_array(TransactionGroupingEngine::Transaction::All.new(event_id: @event.id, search: params[:q], tag_id: @tag&.id).run).page(params[:page]).per(100)
     TransactionGroupingEngine::Transaction::AssociationPreloader.new(transactions: @transactions, event: @event).run!
-    
+
     render :overview
   end
 
