2cd4112843f57421e406774b9dce4c62290c6080
Caleb Denio <cjdenio44@gmail.com>
2023-04-03 10:46:07 -0400

    Revert "April Fools 2023: HCB Platinum (#3734)"
    This reverts commit 98398cb43815bbe3b28451d7120c202b8c76ec0c.

--- db/migrate/20230331194656_add_is_platinum_april_fools2023_to_stripe_card.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class AddIsPlatinumAprilFools2023ToStripeCard < ActiveRecord::Migration[7.0]
-  def change
-    add_column :stripe_cards, :is_platinum_april_fools_2023, :boolean
-  end
-
-end
--- db/migrate/20230401143129_add_seen_platinum_announcement_to_user.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class AddSeenPlatinumAnnouncementToUser < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :seen_platinum_announcement, :boolean, default: false
-  end
-
-end
