fd513ed2794acfa2d3e7bdd6c54f2bef4c882c6b
Max Wofford <max@maxwofford.com>
2023-04-26 19:51:16 -0400

    Rename usage of refund job for Check -> LobCheck

--- app/jobs/refund_sent_check_job.rb
+++ app/jobs/refund_sent_check_job.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
-class RefundSentCheckJob < ApplicationJob
+class RefundSentLobCheckJob < ApplicationJob
   queue_as :default
 
-  def perform(check)
-    return if check.deposited? || check.pending_void? || check.voided?
+  def perform(lob_check)
+    return if lob_check.deposited? || lob_check.pending_void? || lob_check.voided?
 
-    LobCheckMailer.with(check: check).undeposited.deliver_later
+    LobCheckMailer.with(lob_check: lob_check).undeposited.deliver_later
     # LobCheckMailer.with(check: check).undeposited_organizers.deliver_later
-    check.void!
+    lob_check.void!
   end
 
 end
