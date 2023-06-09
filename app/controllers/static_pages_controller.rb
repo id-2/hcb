c6d59c65e99d69bab6a6ffd088f5dd67387df863
yodalightsabr <yoda@yodacode.xyz>
2023-06-09 02:05:22 -0700

    Update app/controllers/static_pages_controller.rb
    Co-authored-by: Gary Tou <gary@garytou.com>

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -128,6 +128,7 @@ class StaticPagesController < ApplicationController
 
 
           pairing = receipt.suggested_pairings.order(distance: :asc).first
+          next if pairing.nil?
 
           {
             hcb_code: pairing.hcb_code,
