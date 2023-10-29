44f6f1644b1cc72bb15d7cec80269fec6d2b9b0c
Sam Poder <sam.r.poder@gmail.com>
2023-10-29 14:21:36 -0700

    Create 20231029211935_add_unique_constraint_to_users_phone_number.rb

---
+++ db/migrate/20231029211935_add_unique_constraint_to_users_phone_number.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class AddUniqueConstraintToUsersPhoneNumber < ActiveRecord::Migration[7.0]
+  def change
+    add_index :users, :phone_number, unique: true
+  end
+
+end
