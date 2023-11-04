d36f895712c6154b427c69cbbf50cf1a70caa8cb
Sam Poder <sam.r.poder@gmail.com>
2023-11-04 09:56:36 -0400

    Update event_factory.rb

--- spec/factories/event_factory.rb
+++ spec/factories/event_factory.rb
@@ -5,6 +5,7 @@ FactoryBot.define do
     name { Faker::Name.unique.name }
     sponsorship_fee { 0 }
     organization_identifier { SecureRandom.hex(30) }
+    point_of_contact_id { create(:user, :make_admin).id }
 
     trait :demo_mode do
       demo_mode { true }
