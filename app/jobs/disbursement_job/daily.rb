511b69250f74a6989bb20bbb3f3d517da3cb8605
Sam Poder <sam.r.poder@gmail.com>
2023-12-22 10:28:30 -0800

    Linting fixes

--- app/jobs/disbursement_job/daily.rb
+++ app/jobs/disbursement_job/daily.rb
@@ -8,15 +8,15 @@ module DisbursementJob
         # 1. Create the raw pending transactions
         rpidt = ::PendingTransactionEngine::RawPendingIncomingDisbursementTransactionService::Disbursement::ImportSingle.new(disbursement:).run
         rpodt = ::PendingTransactionEngine::RawPendingOutgoingDisbursementTransactionService::Disbursement::ImportSingle.new(disbursement:).run
-        
+
         # 2. Canonize the newly added raw pending transactions
         i_cpt = ::PendingTransactionEngine::CanonicalPendingTransactionService::ImportSingle::IncomingDisbursement.new(raw_pending_incoming_disbursement_transaction: rpidt).run
         o_cpt = ::PendingTransactionEngine::CanonicalPendingTransactionService::ImportSingle::OutgoingDisbursement.new(raw_pending_outgoing_disbursement_transaction: rpodt).run
-        
+
         # 3. Map to event
         ::PendingEventMappingEngine::Map::Single::IncomingDisbursement.new(canonical_pending_transaction: i_cpt).run
         ::PendingEventMappingEngine::Map::Single::OutgoingDisbursement.new(canonical_pending_transaction: o_cpt).run
-        
+
         disbursement.mark_approved!(disbursement.fulfilled_by)
       end
     end
--- app/services/disbursement_service/create.rb
+++ app/services/disbursement_service/create.rb
@@ -30,11 +30,11 @@ module DisbursementService
         # 1. Create the raw pending transactions
         rpidt = ::PendingTransactionEngine::RawPendingIncomingDisbursementTransactionService::Disbursement::ImportSingle.new(disbursement:).run
         rpodt = ::PendingTransactionEngine::RawPendingOutgoingDisbursementTransactionService::Disbursement::ImportSingle.new(disbursement:).run
-  
+
         # 2. Canonize the newly added raw pending transactions
         i_cpt = ::PendingTransactionEngine::CanonicalPendingTransactionService::ImportSingle::IncomingDisbursement.new(raw_pending_incoming_disbursement_transaction: rpidt).run
         o_cpt = ::PendingTransactionEngine::CanonicalPendingTransactionService::ImportSingle::OutgoingDisbursement.new(raw_pending_outgoing_disbursement_transaction: rpodt).run
-  
+
         # 3. Map to event
         ::PendingEventMappingEngine::Map::Single::IncomingDisbursement.new(canonical_pending_transaction: i_cpt).run
         ::PendingEventMappingEngine::Map::Single::OutgoingDisbursement.new(canonical_pending_transaction: o_cpt).run
