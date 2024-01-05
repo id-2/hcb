62c89604bd4a5597d507d43c91a293516f829b7d
Sam Poder <sam.r.poder@gmail.com>
2024-01-05 13:10:42 -0700

    Update hcb_code_receipts_mailbox.rb

--- app/mailboxes/hcb_code_receipts_mailbox.rb
+++ app/mailboxes/hcb_code_receipts_mailbox.rb
@@ -27,6 +27,17 @@ class HcbCodeReceiptsMailbox < ApplicationMailbox
     ).run!
 
     return bounce_error if result.empty?
+    
+    text = mail.multipart? ? (mail.text_part ? mail.text_part.body.decoded : nil) : mail.body.decoded
+    
+    rename_to = text.lines.find { |line| line.start_with?("Rename to:") }
+    
+    if rename_to
+      rename_to.slice!('Rename to:')
+      custom_memo = rename_to.strip
+      @hcb_code.canonical_transactions.each { |ct| ct.update!(custom_memo:) }
+      @hcb_code.canonical_pending_transactions.each { |cpt| cpt.update!(custom_memo:) }
+    end
 
     HcbCodeReceiptsMailer.with(
       mail: inbound_email,
