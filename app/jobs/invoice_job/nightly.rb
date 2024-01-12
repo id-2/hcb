2f25ecf75d9d959afa7fa8005671927dc796dd44
Sam Poder <sam.r.poder@gmail.com>
2024-01-12 10:28:45 -0600

    Revert "Delete nightly.rb"
    This reverts commit 4312c4900116f6cadcac10e9597d16df0ab53d69.

---
+++ app/jobs/invoice_job/nightly.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module InvoiceJob
+  class Nightly < ApplicationJob
+    Invoice.paid_v2.each do |invoice|
+      if invoice.local_hcb_code.canonical_transactions.count == 2 || invoice.completed_deprecated? # payout + fee reimbursement
+        invoice.mark_deposited!
+      end
+    end
+
+  end
+
+end
