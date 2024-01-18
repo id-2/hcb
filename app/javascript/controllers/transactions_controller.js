2c22c730571641209e05ef52027e8dfbc1f31653
Sam Poder <sam.r.poder@gmail.com>
2024-01-18 13:58:12 -0800

    prettier!

--- app/javascript/controllers/transactions_controller.js
+++ app/javascript/controllers/transactions_controller.js
@@ -1,33 +1,34 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
-	static targets = ["transaction", "tagger", "formField"];
-	selectedHCBCodes = [];
+  static targets = ['transaction', 'tagger', 'formField']
+  selectedHCBCodes = []
 
-	trackClick(event) {
-		const clickedElement = event.currentTarget;
-		if (event.metaKey || event.ctrlKey) {
-			if(!this.selectedHCBCodes.includes(clickedElement.id)){
-				this.selectedHCBCodes.push(clickedElement.id);
-				clickedElement.classList.add("selected__transaction")
-				if(this.selectedHCBCodes.length > 0){
-					this.taggerTarget.classList.remove("display-none")
-				}
-			}
-			else{
-				this.selectedHCBCodes = this.selectedHCBCodes.filter(x => x != clickedElement.id);
-				clickedElement.classList.remove("selected__transaction")
-				if(this.selectedHCBCodes.length == 0){
-					this.taggerTarget.classList.add("display-none")
-				}
-			}
-			this.updateFormFieldValue()
-		}
-	}
-	
-	updateFormFieldValue() {
-		this.formFieldTargets.map(formField => {
-			formField.value = this.selectedHCBCodes;
-		})
-	}
+  trackClick(event) {
+    const clickedElement = event.currentTarget
+    if (event.metaKey || event.ctrlKey) {
+      if (!this.selectedHCBCodes.includes(clickedElement.id)) {
+        this.selectedHCBCodes.push(clickedElement.id)
+        clickedElement.classList.add('selected__transaction')
+        if (this.selectedHCBCodes.length > 0) {
+          this.taggerTarget.classList.remove('display-none')
+        }
+      } else {
+        this.selectedHCBCodes = this.selectedHCBCodes.filter(
+          x => x != clickedElement.id
+        )
+        clickedElement.classList.remove('selected__transaction')
+        if (this.selectedHCBCodes.length == 0) {
+          this.taggerTarget.classList.add('display-none')
+        }
+      }
+      this.updateFormFieldValue()
+    }
+  }
+
+  updateFormFieldValue() {
+    this.formFieldTargets.map(formField => {
+      formField.value = this.selectedHCBCodes
+    })
+  }
 }
