eb6d0f3008855e16bf3c1bf15c57411197c3644a
Sam Poder <sam.r.poder@gmail.com>
2024-02-28 02:01:33 -0800

    21 offenses corrected. Innocent?

--- app/models/user/payout_method.rb
+++ app/models/user/payout_method.rb
@@ -1,5 +1,9 @@
-module User::PayoutMethod
-  def self.table_name_prefix
-    "user_payout_method_"
+# frozen_string_literal: true
+
+module User
+  module PayoutMethod
+    def self.table_name_prefix
+      "user_payout_method_"
+    end
   end
 end
--- app/models/user/payout_method/ach_transfer.rb
+++ app/models/user/payout_method/ach_transfer.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # == Schema Information
 #
 # Table name: user_payout_method_ach_transfers
@@ -8,10 +10,14 @@
 #  created_at                :datetime         not null
 #  updated_at                :datetime         not null
 #
-class User::PayoutMethod::AchTransfer < ApplicationRecord
-  has_one :user, as: :payment_method
-  has_encrypted :account_number, :routing_number
-  validates :routing_number, format: { with: /\A\d{9}\z/, message: "must be 9 digits" }
-  validates :account_number, format: { with: /\A\d+\z/, message: "must be only numbers" }
+module User
+  module PayoutMethod
+    class AchTransfer < ApplicationRecord
+      has_one :user, as: :payment_method
+      has_encrypted :account_number, :routing_number
+      validates :routing_number, format: { with: /\A\d{9}\z/, message: "must be 9 digits" }
+      validates :account_number, format: { with: /\A\d+\z/, message: "must be only numbers" }
 
+    end
+  end
 end
