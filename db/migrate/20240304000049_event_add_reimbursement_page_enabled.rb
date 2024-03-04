0555d95f176b7dab723675f499c4d622587f2fae
Soar1826 <34289582+Soar1826@users.noreply.github.com>
2024-03-04 00:43:57 +0000

    rename column because I misunderstood quick expense

--- db/migrate/20240304000049_event_add_reimbursement_page_enabled.rb
+++ db/migrate/20240304000049_event_add_reimbursement_page_enabled.rb
@@ -1,5 +1,5 @@
 class EventAddReimbursementPageEnabled < ActiveRecord::Migration[7.0]
   def change
-    add_column :events, :self_service_reimbursement_page_enabled, :boolean, default: false
+    add_column :events, :public_reimbursement_page_enabled, :boolean, default: false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -743,7 +743,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_04_000049) do
     t.text "donation_thank_you_message"
     t.text "donation_reply_to_email"
     t.string "postal_code"
-    t.boolean "self_service_reimbursement_page_enabled", default: false
+    t.boolean "public_reimbursement_page_enabled", default: false
     t.index ["club_airtable_id"], name: "index_events_on_club_airtable_id", unique: true
     t.index ["partner_id", "organization_identifier"], name: "index_events_on_partner_id_and_organization_identifier", unique: true
     t.index ["partner_id"], name: "index_events_on_partner_id"
