cdba28be5ec95d441397b6396a061e64458d0469
Sam Poder <sampoder@berkeley.edu>
2024-03-05 10:09:32 -0800

    Don't Fallback for Amazon SES Emails

--- app/mailboxes/fallback_mailbox.rb
+++ app/mailboxes/fallback_mailbox.rb
@@ -4,7 +4,10 @@ class FallbackMailbox < ApplicationMailbox
   # mail --> Mail object, this actual email
   # inbound_email => ActionMailbox::InboundEmail record --> the active storage record
 
+  blacklist = ["mailer-daemon@amazonses.com"]
+
   def process
+    return if (blacklist & from.collect(&:downcase)).any?
     bounce_with MailboxMailer.forward(inbound_email:, to: "hcb@hackclub.com")
   end
 
