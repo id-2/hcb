d2a3a909525a3a20333e3ac81f9f748db4996bb9
Gary Tou <gary@garytou.com>
2024-03-13 17:42:03 -0700

    `PaymentRecipientPolicy`: Use new helpers

--- app/policies/payment_recipient_policy.rb
+++ app/policies/payment_recipient_policy.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class PaymentRecipientPolicy < ApplicationPolicy
-  def destroy?
-    user&.admin? || record.event.users.include?(user)
+  permit_admins_to def destroy?
+    record.event.users.include?(user)
   end
 
 end
