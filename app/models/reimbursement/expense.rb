ce802b80b4d62b4514694ec383d1bbfa513a939d
Gary Tou <gary@garytou.com>
2024-03-19 09:22:50 -0700

    Remove extra whitespace in annotation
    ```
    bundle exec annotate --models
    ```

--- app/models/reimbursement/expense.rb
+++ app/models/reimbursement/expense.rb
@@ -4,18 +4,18 @@
 #
 # Table name: reimbursement_expenses
 #
-#  id                         :bigint           not null, primary key
-#  aasm_state                 :string
-#  amount_cents               :integer          default(0), not null
-#  approved_at                :datetime
-#  description                :text
-#  expense_number             :integer          default(1), not null
-#  memo                       :text
-#  created_at                 :datetime         not null
-#  updated_at                 :datetime         not null
-#  approved_by_id             :bigint
-#  reimbursement_report_id    :bigint           not null
-#  sequential_id              :integer          not null
+#  id                      :bigint           not null, primary key
+#  aasm_state              :string
+#  amount_cents            :integer          default(0), not null
+#  approved_at             :datetime
+#  description             :text
+#  expense_number          :integer          default(1), not null
+#  memo                    :text
+#  created_at              :datetime         not null
+#  updated_at              :datetime         not null
+#  approved_by_id          :bigint
+#  reimbursement_report_id :bigint           not null
+#  sequential_id           :integer          not null
 #
 # Indexes
 #
