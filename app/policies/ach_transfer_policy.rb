0b5edbefb851ed1f13d2c3475e851491fba71245
Gary Tou <gary@garytou.com>
2024-03-26 22:11:11 -0700

    Example Usage (`AchTransfer`)

--- app/policies/ach_transfer_policy.rb
+++ app/policies/ach_transfer_policy.rb
@@ -1,59 +1,35 @@
 # frozen_string_literal: true
 
 class AchTransferPolicy < ApplicationPolicy
-  def index?
-    user&.admin?
+  get(:event) { record&.event }
+
+  policy_for :index?,
+             :start_approval?,
+             :approve?,
+             :reject? do
+    ⚡
   end
 
-  def new?
-    is_public? || user_who_can_transfer?
+  policy_for :new?, :show? do
+    🔎 || user_can_transfer?
   end
 
   def create?
-    user_who_can_transfer? && !record.event.demo_mode && !record.event.outernet_guild?
-  end
-
-  def show?
-    # Semantically, this should be admin_or_manager?, right?
-    is_public? || user_who_can_transfer?
+    user_can_transfer? && !event.demo_mode && !event.outernet_guild?
   end
 
   def view_account_routing_numbers?
-    admin_or_manager?
+    ⚡ || 👔
   end
 
-  def cancel?
-    user_who_can_transfer?
-  end
-
-  def transfer_confirmation_letter?
-    user_who_can_transfer?
-  end
-
-  def start_approval?
-    user&.admin?
-  end
-
-  def approve?
-    user&.admin?
-  end
-
-  def reject?
-    user&.admin?
+  policy_for :cancel?, :transfer_confirmation_letter? do
+    user_can_transfer?
   end
 
   private
 
-  def user_who_can_transfer?
-    user&.admin? || EventPolicy.new(user, record.event).new_transfer?
-  end
-
-  def admin_or_manager?
-    user&.admin? || OrganizerPosition.find_by(user:, event: record.event)&.manager?
-  end
-
-  def is_public?
-    record.event.is_public?
+  def user_can_transfer?
+    user&.admin? || EventPolicy.new(user, event).new_transfer?
   end
 
 end
