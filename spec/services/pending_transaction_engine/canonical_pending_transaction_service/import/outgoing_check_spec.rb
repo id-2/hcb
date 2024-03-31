8ac57b1546744a4e3463c255d48b22e03f9fb0da
Riadh <22998716+KiKoS0@users.noreply.github.com>
2024-03-31 21:56:24 +0200

    Implement spec for PendingTransactionEngine::CanonicalPendingTransactionService::Import::OutgoingCheck

---
+++ spec/services/pending_transaction_engine/canonical_pending_transaction_service/import/outgoing_check_spec.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe PendingTransactionEngine::CanonicalPendingTransactionService::Import::OutgoingCheck do
+
+  context "when there is a pending outgoing check transaction ready for processing" do
+    it "processes into a CanonicalPendingTransaction" do
+      expect(RawPendingOutgoingCheckTransaction.count).to eq(0)
+
+      check = create(:check)
+      raw_pending_outgoing_check_transaction = create(:raw_pending_outgoing_check_transaction,
+                                                      check_transaction_id: check.id,
+                                                      date_posted: Date.current)
+
+      expect do
+        described_class.new.run
+      end.to change { CanonicalPendingTransaction.count }.by(1)
+    end
+  end
+
+  context "when there are previously processed raw pending outgoing check transactions" do
+    let(:raw_pending_outgoing_check_transaction) { create(:raw_pending_outgoing_check_transaction) }
+
+    before do
+      _processed_outgoing_check_canonical_pending_transaction = create(:canonical_pending_transaction, raw_pending_outgoing_check_transaction:)
+    end
+
+    it "ignores it when processing" do
+      expect do
+        described_class.new.run
+      end.to change { CanonicalPendingTransaction.count }.by(0)
+    end
+
+    context "when there are also ready to process raw pending outgoing check transactions" do
+      it "processes into a CanonicalPendingTransaction" do
+        check = create(:check)
+        new_check_transaction = create(:raw_pending_outgoing_check_transaction,
+                                       check_transaction_id: check.id,
+                                       date_posted: Date.current,
+                                       amount_cents: 1000)
+
+        expect(RawPendingOutgoingCheckTransaction.count).to eq(2)
+
+        expect do
+          described_class.new.run
+        end.to change { CanonicalPendingTransaction.count }.by(1)
+
+        pending_transaction = CanonicalPendingTransaction.last
+        expect(pending_transaction.raw_pending_outgoing_check_transaction_id).to eq(new_check_transaction.id)
+        expect(pending_transaction.amount_cents).to eq(new_check_transaction.amount_cents)
+      end
+    end
+  end
+
+end
