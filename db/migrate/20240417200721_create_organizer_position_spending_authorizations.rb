257dbb67bc1c93c5f2493cc83a3715a8f23a3861
Malted <malted@malted.dev>
2024-04-19 12:08:56 -0400

    Rename index on migration

--- db/migrate/20240417200721_create_organizer_position_spending_authorizations.rb
+++ db/migrate/20240417200721_create_organizer_position_spending_authorizations.rb
@@ -2,7 +2,7 @@ class CreateOrganizerPositionSpendingAuthorizations < ActiveRecord::Migration[7.
   def change
     create_table :organizer_position_spending_authorizations do |t|
       t.references :organizer_position, null: false, foreign_key: true, index: { name: "idx_org_pos_spend_auths_on_org_pos_id" }
-      t.references :authorized_by, null: false, foreign_key: { to_table: :users }
+      t.references :authorized_by, null: false, foreign_key: { to_table: :users }, index: { name: "idx_org_pos_spend_auths_on_authed_by_id" }
       t.integer :amount_cents, null: false
       t.text :memo
 
