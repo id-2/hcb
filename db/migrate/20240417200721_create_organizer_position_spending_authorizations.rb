6b5e055ea4ab8a088915db00270792e6d38c67df
Malted <malted@malted.dev>
2024-04-24 13:18:03 -0400

    Rename spending auth index to spending allowances

--- db/migrate/20240417200721_create_organizer_position_spending_authorizations.rb
+++ db/migrate/20240417200721_create_organizer_position_spending_authorizations.rb
@@ -1,8 +1,8 @@
 class CreateOrganizerPositionSpendingAuthorizations < ActiveRecord::Migration[7.0]
   def change
     create_table :organizer_position_spending_authorizations do |t|
-      t.references :organizer_position, null: false, foreign_key: true, index: { name: "idx_org_pos_spend_auths_on_org_pos_id" }
-      t.references :authorized_by, null: false, foreign_key: { to_table: :users }, index: { name: "idx_org_pos_spend_auths_on_authed_by_id" }
+      t.references :organizer_position, null: false, foreign_key: true, index: { name: "idx_org_pos_spend_allows_on_org_pos_id" }
+      t.references :authorized_by, null: false, foreign_key: { to_table: :users }, index: { name: "idx_org_pos_spend_allows_on_authed_by_id" }
       t.integer :amount_cents, null: false
       t.text :memo
 
