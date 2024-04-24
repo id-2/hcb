1b2b53aa1ea2f30186513d8bcacb30917421eac3
Malted <malted@malted.dev>
2024-04-24 13:16:18 -0400

    Edit schema for org pos spending controls

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_17_200721) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_24_170843) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "plpgsql"
@@ -1271,6 +1271,16 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_17_200721) do
     t.index ["organizer_position_id"], name: "idx_org_pos_spend_auths_on_org_pos_id"
   end
 
+  create_table "organizer_position_spending_controls", force: :cascade do |t|
+    t.boolean "active"
+    t.datetime "started_at"
+    t.datetime "ended_at"
+    t.bigint "organizer_position_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["organizer_position_id"], name: "idx_org_pos_spend_ctrls_on_org_pos_id"
+  end
+
   create_table "organizer_positions", force: :cascade do |t|
     t.bigint "user_id"
     t.bigint "event_id"
@@ -2029,6 +2039,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_17_200721) do
   add_foreign_key "organizer_position_invites", "users", column: "sender_id"
   add_foreign_key "organizer_position_spending_authorizations", "organizer_positions"
   add_foreign_key "organizer_position_spending_authorizations", "users", column: "authorized_by_id"
+  add_foreign_key "organizer_position_spending_controls", "organizer_positions"
   add_foreign_key "organizer_positions", "events"
   add_foreign_key "organizer_positions", "users"
   add_foreign_key "partner_donations", "events"
