9da0ee64babe41a4738a006478d44705cc81d47a
Malted <malted@malted.dev>
2024-04-24 13:16:39 -0400

    Create Control model

---
+++ app/models/organizer_position/spending/control.rb
@@ -0,0 +1,12 @@
+class OrganizerPosition::Spending::Control < ApplicationRecord
+  belongs_to :organizer_position
+  has_many :allowances
+
+  validates :one_active_control
+
+  def one_active_control
+    if organizer_position.spending_controls.where(active: true).size > 1
+      errors.add(:organizer_position, "may only have one active spending control")
+    end
+  end
+end
