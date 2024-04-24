63e7ad0a378fca25be9583c8603e84a5211abe4e
Malted <malted@malted.dev>
2024-04-24 18:00:24 -0400

    Use validate instead of validates

--- app/models/organizer_position/spending/control.rb
+++ app/models/organizer_position/spending/control.rb
@@ -2,7 +2,9 @@ class OrganizerPosition::Spending::Control < ApplicationRecord
   belongs_to :organizer_position
   has_many :allowances
 
-  validates :one_active_control
+  scope :active_spending_control, -> { where(active: true) }
+
+  validate :one_active_control
 
   def one_active_control
     if organizer_position.spending_controls.where(active: true).size > 1
