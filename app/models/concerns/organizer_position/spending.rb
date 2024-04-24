98e1c7d19697c436d37ae1881cb4e803d4b11c5a
Malted <malted@malted.dev>
2024-04-24 18:35:52 -0400

    Move associations to concern

--- app/models/concerns/organizer_position/spending.rb
+++ app/models/concerns/organizer_position/spending.rb
@@ -2,8 +2,9 @@ class OrganizerPosition
   module Spending
     extend ActiveSupport::Concern
     included do
-      has_many :spending_controls
-      has_one :active_spending_control, -> { where(active: true) }
+      has_many :spending_allowances, class_name: "OrganizerPosition::Spending::Allowance"
+      has_many :spending_controls, class_name: "OrganizerPosition::Spending::Control"
+      # has_one :active_spending_control, -> { where(spending_controls: { active: true }) }
     end
 
     def spending_control_enabled?
