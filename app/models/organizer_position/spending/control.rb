04eef9bbfe7ae6bc9c246ba518b7487c5ab693d6
Malted <malted@malted.dev>
2024-04-25 12:42:03 -0400

    Annotate spending control model

--- app/models/organizer_position/spending/control.rb
+++ app/models/organizer_position/spending/control.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: organizer_position_spending_controls
+#
+#  id                    :bigint           not null, primary key
+#  active                :boolean
+#  ended_at              :datetime
+#  started_at            :datetime
+#  created_at            :datetime         not null
+#  updated_at            :datetime         not null
+#  organizer_position_id :bigint           not null
+#
+# Indexes
+#
+#  idx_org_pos_spend_ctrls_on_org_pos_id  (organizer_position_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (organizer_position_id => organizer_positions.id)
+#
 class OrganizerPosition::Spending::Control < ApplicationRecord
   belongs_to :organizer_position
   has_many :allowances
