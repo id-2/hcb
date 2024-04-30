209d733ba783d208e8d7efd9f6454d3302ca136f
Malted <malted@malted.dev>
2024-04-30 14:39:56 -0400

    Update allowance annotations

--- app/models/organizer_position/spending/allowance.rb
+++ app/models/organizer_position/spending/allowance.rb
@@ -2,23 +2,23 @@
 #
 # Table name: organizer_position_spending_allowances
 #
-#  id                    :bigint           not null, primary key
-#  amount_cents          :integer          not null
-#  memo                  :text
-#  created_at            :datetime         not null
-#  updated_at            :datetime         not null
-#  authorized_by_id      :bigint           not null
-#  organizer_position_id :bigint           not null
+#  id                                     :bigint           not null, primary key
+#  amount_cents                           :integer          not null
+#  memo                                   :text
+#  created_at                             :datetime         not null
+#  updated_at                             :datetime         not null
+#  authorized_by_id                       :bigint           not null
+#  organizer_position_spending_control_id :bigint           not null
 #
 # Indexes
 #
 #  idx_org_pos_spend_allows_on_authed_by_id  (authorized_by_id)
-#  idx_org_pos_spend_allows_on_org_pos_id    (organizer_position_id)
+#  idx_org_pos_spend_allows_on_org_pos_id    (organizer_position_spending_control_id)
 #
 # Foreign Keys
 #
 #  fk_rails_...  (authorized_by_id => users.id)
-#  fk_rails_...  (organizer_position_id => organizer_positions.id)
+#  fk_rails_...  (organizer_position_spending_control_id => organizer_position_spending_controls.id)
 #
 module OrganizerPosition::Spending
   class Allowance < ApplicationRecord
