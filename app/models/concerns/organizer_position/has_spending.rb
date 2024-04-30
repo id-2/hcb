6474ae01d128a8985870bd6b9a1942d0476c0d02
Malted <malted@malted.dev>
2024-04-30 14:35:14 -0400

    Remove `spending_allowances` from `OrganizerPosition::Spending`

--- app/models/concerns/organizer_position/has_spending.rb
+++ app/models/concerns/organizer_position/has_spending.rb
@@ -2,7 +2,6 @@ class OrganizerPosition
   module HasSpending
     extend ActiveSupport::Concern
     included do
-      has_many :spending_allowances, class_name: "OrganizerPosition::Spending::Allowance"
       has_many :spending_controls, class_name: "OrganizerPosition::Spending::Control"
       has_one :active_spending_control, -> { where(active: true) }, class_name: "OrganizerPosition::Spending::Control"
     end
