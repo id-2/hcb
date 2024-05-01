494029fe47da9880837b97c4799c7033e768ec2d
Malted <malted@malted.dev>
2024-05-01 15:23:19 -0400

    Move spending item sorting to helper method

--- app/helpers/organizer_position/spending/allowances_helper.rb
+++ app/helpers/organizer_position/spending/allowances_helper.rb
@@ -1,5 +1,18 @@
 module OrganizerPosition::Spending::AllowancesHelper
 
+  def sorted_spending_items(control)
+    transactions = @op.stripe_cards.map{ |card| card.canonical_pending_transactions }.flatten
+    allowances = control.organizer_position_spending_allowances
+
+    if !allowances || params[:filter] == "transactions"
+      @spending_items = transactions.sort_by(&:created_at).reverse
+    elsif !transactions || params[:filter] == "allowances"
+      @spending_items = allowances.order(created_at: :desc)
+    else
+      @spending_items = (transactions + allowances).sort_by(&:created_at).reverse
+    end
+  end
+  
   def render_spending_items(items)
     if items.count == 0
       concat blankslate("Nothing to see here")
