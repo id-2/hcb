49ed2e46c13552febd2280b322575d1eb3ba6f58
Malted <malted@malted.dev>
2024-05-22 16:18:06 -0400

    Use partials

--- app/helpers/organizer_position/spending/allowances_helper.rb
+++ app/helpers/organizer_position/spending/allowances_helper.rb
@@ -38,28 +38,13 @@ module OrganizerPosition::Spending::AllowancesHelper
             end)
             concat(content_tag(:tbody) do
               items.each do |i|
-                concat render_spending_item(i)
+                concat(render("organizer_positions/spending/controls/spending_item", item: i))
+                # concat render_spending_item(i)
               end
             end)
           end)
         end)
       end
-    end
-  end
-
-  def render_spending_item(item)
-    item_user = item.is_a?(OrganizerPosition::Spending::Allowance) ? item.authorized_by.user : item.stripe_cardholder.user
-
-    capture do
-      concat(content_tag(:tr, class: item.is_a?(OrganizerPosition::Spending::Allowance) ? "transaction--positive" : "transaction--negative") do
-        concat content_tag(:td, format_date(item.created_at))
-        concat content_tag(:td, item.memo, style: "max-width: 400px; overflow: hidden; text-overflow: elipsis;")
-        concat(content_tag(:td, class: "flex items-center g1") do
-          concat avatar_for(item_user, 24)
-          concat item_user.name
-        end)
-        concat content_tag(:td, render_money(item.amount_cents))
-      end)
     end
   end
 
