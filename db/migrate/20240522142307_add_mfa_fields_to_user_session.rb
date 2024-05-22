4a24fc8df6754429bede82ec54d879a3b50cb6a2
Sam Poder <39828164+sampoder@users.noreply.github.com>
2024-05-22 10:33:44 -0400

    Update 20240522142307_add_mfa_fields_to_user_session.rb

--- db/migrate/20240522142307_add_mfa_fields_to_user_session.rb
+++ db/migrate/20240522142307_add_mfa_fields_to_user_session.rb
@@ -1,4 +1,6 @@
 class AddMfaFieldsToUserSession < ActiveRecord::Migration[7.1]
   def change
+    add_column :user_sessions, :aasm_state, :string
+    add_column :user_sessions, :permissions, :integer, default: 0, null: false
   end
 end
