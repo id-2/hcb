e20143b61cef9bce14adc13d394de663ec0206d6
Malted <malted@malted.dev>
2024-05-24 14:13:09 -0400

    Refactor control policy

--- app/policies/organizer_position/spending/control_policy.rb
+++ app/policies/organizer_position/spending/control_policy.rb
@@ -1,13 +1,18 @@
 class OrganizerPosition::Spending::ControlPolicy < ApplicationPolicy
   def new?
-    (user.admin? ||
-      OrganizerPosition.find_by(user:, event: record.organizer_position.event).manager?) &&
-      record.active
+    record.active && (
+      user.admin? || (
+        OrganizerPosition.find_by(user:, event: record.organizer_position.event).manager? &&
+        user != record.organizer_position.user
+      )
+    )
   end
 
   def destroy?
     user.admin? ||
       OrganizerPosition.find_by(user:, event: record.organizer_position.event).manager?
   end
+
+  private
 
 end
