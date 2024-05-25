3e77e135898038149f4c11f265209370793e8faa
Malted <malted@malted.dev>
2024-05-25 11:20:24 -0400

    Demodulise controlpolicy

--- app/policies/organizer_position/spending/control_policy.rb
+++ app/policies/organizer_position/spending/control_policy.rb
@@ -1,16 +1,21 @@
-class OrganizerPosition::Spending::ControlPolicy < ApplicationPolicy
-  def new?
-    user.admin? || (
-      OrganizerPosition.find_by(user:, event: record.organizer_position.event).manager? &&
-      user != record.organizer_position.user
-    )
-  end
+class OrganizerPosition
+  module Spending
+    class ControlPolicy < ApplicationPolicy
+      def new?
+        user.admin? || (
+          OrganizerPosition.find_by(user:, event: record.organizer_position.event).manager? &&
+          user != record.organizer_position.user
+        )
+      end
 
-  def destroy?
-    user.admin? ||
-      OrganizerPosition.find_by(user:, event: record.organizer_position.event).manager?
-  end
+      def destroy?
+        user.admin? ||
+          OrganizerPosition.find_by(user:, event: record.organizer_position.event).manager?
+      end
 
-  private
+      private
+
+    end
+  end
 
 end
