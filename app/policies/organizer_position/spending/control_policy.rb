7f5d0648f84e3eff4a3a82c6ce3fbc2daca24df2
Malted <malted@malted.dev>
2024-05-28 18:03:22 -0400

    Refactor policy

--- app/policies/organizer_position/spending/control_policy.rb
+++ app/policies/organizer_position/spending/control_policy.rb
@@ -1,21 +1,25 @@
 class OrganizerPosition
   module Spending
     class ControlPolicy < ApplicationPolicy
+
+      def index?
+        user.admin? || manager? || user == record.organizer_position.user
+      end
+
       def new?
-        record.active && (
-          user.admin? || (
-            OrganizerPosition.find_by(user:, event: record.organizer_position.event).manager? &&
-            user != record.organizer_position.user
-          )
-        )
+          user.admin? || (manager? && user != record.organizer_position.user)
       end
 
       def destroy?
-        user.admin? ||
-          OrganizerPosition.find_by(user:, event: record.organizer_position.event).manager?
+        user.admin? || manager?
       end
 
       private
+
+      def manager?
+        # Whether the current user is a manager, not the target of the control
+        OrganizerPosition.find_by(user:, event: record.organizer_position.event).manager?
+      end
 
     end
   end
