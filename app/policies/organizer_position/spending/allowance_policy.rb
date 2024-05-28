51d9c0c65cf49b8d3bc96996e27435cf4d90a856
Malted <malted@malted.dev>
2024-05-28 15:31:50 -0400

    Modulise allowance policy

--- app/policies/organizer_position/spending/allowance_policy.rb
+++ app/policies/organizer_position/spending/allowance_policy.rb
@@ -1,16 +1,21 @@
-class OrganizerPosition::Spending::AllowancePolicy < ApplicationPolicy
-  def index?
-    # `record` in this method is an OrganizerPosition
-    user.admin? || record.manager? || user == record.user
-  end
+class OrganizerPosition
+  module Spending
+    class AllowancePolicy < ApplicationPolicy
+      def index?
+        # `record` in this method is an OrganizerPosition
+        user.admin? || record.manager? || user == record.user
+      end
 
-  def new?
-    true
-  end
+      def new?
+        true
+      end
 
-  def create?
-    user.admin? ||
-      OrganizerPosition.find_by(user:, event: record.event)&.manager?
+      def create?
+        user.admin? ||
+          OrganizerPosition.find_by(user:, event: record.event)&.manager?
+      end
+
+    end
   end
 
 end
