039ed02c435eefaf038e9b3d5051560dbe29bc3c
Malted <malted@malted.dev>
2024-05-28 15:32:07 -0400

    Modulise control policy

--- app/policies/organizer_position/spending/control_policy.rb
+++ app/policies/organizer_position/spending/control_policy.rb
@@ -1,18 +1,23 @@
-class OrganizerPosition::Spending::ControlPolicy < ApplicationPolicy
-  def new?
-    record.active && (
-      user.admin? || (
-        OrganizerPosition.find_by(user:, event: record.organizer_position.event).manager? &&
-        user != record.organizer_position.user
-      )
-    )
-  end
+class OrganizerPosition
+  module Spending
+    class ControlPolicy < ApplicationPolicy
+      def new?
+        record.active && (
+          user.admin? || (
+            OrganizerPosition.find_by(user:, event: record.organizer_position.event).manager? &&
+            user != record.organizer_position.user
+          )
+        )
+      end
 
-  def destroy?
-    user.admin? ||
-      OrganizerPosition.find_by(user:, event: record.organizer_position.event).manager?
-  end
+      def destroy?
+        user.admin? ||
+          OrganizerPosition.find_by(user:, event: record.organizer_position.event).manager?
+      end
 
-  private
+      private
+
+    end
+  end
 
 end
