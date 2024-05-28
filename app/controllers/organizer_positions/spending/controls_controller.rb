f62a3e60dc25732ed7ceadb5b05e4c0bf757420e
Malted <malted@malted.dev>
2024-05-28 15:55:59 -0400

    Rename op to organizer position

--- app/controllers/organizer_positions/spending/controls_controller.rb
+++ app/controllers/organizer_positions/spending/controls_controller.rb
@@ -4,42 +4,42 @@ module OrganizerPositions
       before_action :set_organizer_position
 
       def index
-        @provisional_control = OrganizerPosition::Spending::Control.new(organizer_position: @op)
+        @provisional_control = OrganizerPosition::Spending::Control.new(organizer_position: @organizer_position)
 
         authorize @provisional_control
 
-        @active_control = @op.active_spending_control
-        @inactive_control_count = @op.spending_controls.where(active: false).count
+        @active_control = @organizer_position.active_spending_control
+        @inactive_control_count = @organizer_position.spending_controls.where(active: false).count
       end
 
       def new
         attributes = filtered_params
         attributes[:active] = true
 
-        @control = @op.spending_controls.new(attributes)
+        @control = @organizer_position.spending_controls.new(attributes)
 
         authorize @control
 
         if @control.save
           flash[:success] = "Spending control successfully created!"
-          redirect_to event_organizer_allowances_path organizer_id: @op.user.slug
+          redirect_to event_organizer_allowances_path organizer_id: @organizer_position.user.slug
         else
           render :new, status: :unprocessable_entity
         end
       end
 
       def destroy
-        authorize @op.active_spending_control
+        authorize @organizer_position.active_spending_control
 
-        if active_control = @op.active_spending_control
+        if active_control = @organizer_position.active_spending_control
           if active_control.organizer_position_spending_allowances.count == 0
             active_control.destroy
           else
             active_control.update(active: false, ended_at: Time.current)
           end
 
-          flash[:success] = "Spending controls disabled for #{@op.user.name}!"
-          redirect_to event_organizer_allowances_path organizer_id: @op.user.slug
+          flash[:success] = "Spending controls disabled for #{@organizer_position.user.name}!"
+          redirect_to event_organizer_allowances_path organizer_id: @organizer_position.user.slug
         else
           flash[:error] = "There is no active spending control to destroy"
           redirect_to root_path
@@ -52,9 +52,9 @@ module OrganizerPositions
         @event = Event.friendly.find(params[:event_id])
         begin
           @user = User.friendly.find(params[:organizer_id])
-          @op = OrganizerPosition.find_by!(event: @event, user: @user)
+          @organizer_position = OrganizerPosition.find_by!(event: @event, user: @user)
         rescue ActiveRecord::RecordNotFound
-          @op = OrganizerPosition.find_by!(event: @event, id: params[:organizer_id])
+          @organizer_position = OrganizerPosition.find_by!(event: @event, id: params[:organizer_id])
         end
       end
 
