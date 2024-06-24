c4e2dc8f6d47b553e9597be1b91d3d889b1cf155
Ben <malted@malted.dev>
2024-06-24 14:05:58 -0400

    Discard changes to app/models/login_code.rb

--- app/models/login_code.rb
+++ app/models/login_code.rb
@@ -19,6 +19,10 @@
 #  index_login_codes_on_code     (code)
 #  index_login_codes_on_user_id  (user_id)
 #
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 class LoginCode < ApplicationRecord
   EXPIRATION = 15.minutes
 
