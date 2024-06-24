5a5d30e5ed47b8ebb023eb9c226f7819e6927c5d
Ben <malted@malted.dev>
2024-06-24 14:07:36 -0400

    Discard changes to app/models/webauthn_credential.rb

--- app/models/webauthn_credential.rb
+++ app/models/webauthn_credential.rb
@@ -18,6 +18,10 @@
 #
 #  index_webauthn_credentials_on_user_id  (user_id)
 #
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 class WebauthnCredential < ApplicationRecord
   belongs_to :user
 
