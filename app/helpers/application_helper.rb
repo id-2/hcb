c1d456c5e22301e390aa9646c7a0ea265afb9fb1
Manu G <manusvathgurudath@gmail.com>
2024-08-06 18:49:59 +0000

    stuff from yesterday

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -64,15 +64,14 @@ module ApplicationHelper
                 **options
   end
 
-  def blankslate(heading, subheading = nil, image = "", options = { screen: false, card: false }, &block)
+  def blankslate(heading, subheading = nil, options = { screen: false, card: false }, &block)
     other_options = options.except(:class)
-    content_tag :div, class: "rounded-lg flex p-10 py-12 justify-center z-10 items-center gap-10 blankslate-card #{options[:class]} #{options[:screen] ? "blankslate-screen" : "border max-w-sm w-full mx-auto"}", **other_options do
-      image_tag(image, class: "w-52 dark:invert rounded-xl mb-2") +
-        content_tag(:div, class: "text-lg font-bold", **other_options) do
-          content_tag(:div, heading, class: "text-3xl font-bold my-2") +
-            content_tag(:div, subheading, class: "text-md text-muted mb-5") +
-            (block_given? ? capture(&block) : "")
-        end
+    content_tag :div, class: "rounded-lg flex p-10 justify-center z-10 items-center gap-10 blankslate-card #{options[:class]} #{options[:screen] ? "blankslate-screen" : "border max-w-sm w-full mx-auto"}", **other_options do
+      content_tag(:div, class: "text-lg font-bold", **other_options) do
+        content_tag(:div, heading, class: "text-3xl font-bold my-2") +
+          content_tag(:div, subheading, class: "text-md text-muted mb-5") +
+          (block_given? ? capture(&block) : "")
+      end
     end
   end
 
