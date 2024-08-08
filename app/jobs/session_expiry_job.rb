11f2f0bbb4dd4e2d34ce610a1988625fd668efd4
Malted <malted@malted.dev>
2024-08-08 12:27:32 -0400

    Rename session expiry job

--- app/jobs/session_expiry_job.rb
+++ app/jobs/session_expiry_job.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class SendAdminRemindersJob < ApplicationJob
+class SessionExpiryJob < ApplicationJob
   queue_as :low
   def perform
     UserSession.joins(:user).where("user_sessions.last_seen_at + (users.session_validity_preference * interval '1 minute') < NOW()").each { |session|
