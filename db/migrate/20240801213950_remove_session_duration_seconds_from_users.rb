02aba241551cb5e6fba0ef7d09a4d3a2ae07d0a1
Malted <malted@malted.dev>
2024-08-08 11:26:21 -0400

    rm migration

--- db/migrate/20240801213950_remove_session_duration_seconds_from_users.rb
+++
@@ -1,8 +0,0 @@
-class RemoveSessionDurationSecondsFromUsers < ActiveRecord::Migration[7.1]
-  def change
-    safety_assured do
-      remove_column :users, :session_duration_seconds, :integer
-    end
-  end
-
-end
