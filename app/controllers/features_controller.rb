084a99477181ebe78d7466ce14c82a5fbe7aff24
Manu G <manusvathgurudath@gmail.com>
2024-08-28 07:39:34 +0000

    Improve HCB 🏦📈✨

--- app/controllers/features_controller.rb
+++ app/controllers/features_controller.rb
@@ -14,7 +14,6 @@ class FeaturesController < ApplicationController
     spending_controls_2024_06_03: %w[✅ ❌ 💷],
     ai_memos_2024_06_20: %w[✨ 🔮 🪄],
     cash_withdrawals_2024_08_07: %w[💵 💴 💶 💷],
-    org_switcher_2024_01_31: %w[🔄 🏢 ⬇️],
     two_factor_authentication_2024_05_22: %w[🔒],
     totp_2024_06_13: %w[🔒 ⏰],
   }.freeze
