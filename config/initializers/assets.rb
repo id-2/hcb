b1bf3700144b860c450827eec2046c9947efe43e
Manu G <manusvathgurudath@gmail.com>
2024-08-28 07:13:29 +0000

    Improve HCB 🏦📈✨

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -17,6 +17,6 @@ Rails.application.config.assets.paths << Rails.root.join("node_modules")
 
 # (msw) Load darkmode seperately so we can prevent flashing the user with a
 # bright screen onload
-Rails.application.config.assets.precompile += %w(admin.js)
+Rails.application.config.assets.precompile += %w(dark.js admin.js)
 
 Rails.application.config.assets.css_compressor = nil
