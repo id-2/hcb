79780b1a88ef1ba62e32a150104b043d06fe79ca
Malted <malted@malted.dev>
2024-09-11 19:03:27 -0400

    Revert schema changes

--- db/schema.rb
+++ db/schema.rb
@@ -376,7 +376,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_09_163220) do
     t.bigint "grant_id"
     t.bigint "reimbursement_expense_payout_id"
     t.bigint "paypal_transfer_id"
-    t.bigint "reimbursement_payout_holding_id"
     t.index ["ach_payment_id"], name: "index_canonical_pending_transactions_on_ach_payment_id"
     t.index ["check_deposit_id"], name: "index_canonical_pending_transactions_on_check_deposit_id"
     t.index ["grant_id"], name: "index_canonical_pending_transactions_on_grant_id"
@@ -393,7 +392,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_09_163220) do
     t.index ["raw_pending_partner_donation_transaction_id"], name: "index_canonical_pending_txs_on_raw_pending_partner_dntn_tx_id"
     t.index ["raw_pending_stripe_transaction_id"], name: "index_canonical_pending_txs_on_raw_pending_stripe_tx_id"
     t.index ["reimbursement_expense_payout_id"], name: "index_canonical_pending_txs_on_reimbursement_expense_payout_id"
-    t.index ["reimbursement_payout_holding_id"], name: "index_canonical_pending_txs_on_reimbursement_payout_holding_id"
     t.check_constraint "fronted IS NOT NULL", name: "canonical_pending_transactions_fronted_null"
   end
 
@@ -1787,7 +1785,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_09_163220) do
 
   create_table "reimbursement_reports", force: :cascade do |t|
     t.bigint "user_id", null: false
-    t.bigint "event_id"
+    t.bigint "event_id", null: false
     t.bigint "invited_by_id"
     t.text "invite_message"
     t.text "name"
