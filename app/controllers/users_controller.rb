b3828a3c2a74ab77484b22ccbbba157fa84cea9f
Malted <malted@malted.dev>
2024-09-19 09:58:43 -0400

    Add new parameter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -346,6 +346,7 @@ class UsersController < ApplicationController
       :profile_picture,
       :pretend_is_not_admin,
       :sessions_reported,
+      :session_validity_preference,
       :receipt_report_option,
       :birthday,
       :seasonal_themes_enabled,
