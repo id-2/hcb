bf1b87ea1e45ae5309d02efbd940dafc0cff54c4
Manu G <manusvathgurudath@gmail.com>
2024-10-17 15:11:24 +0000

    Fix chart line colors for dark mode

--- app/javascript/components/home/Categories.js
+++ app/javascript/components/home/Categories.js
@@ -105,7 +105,7 @@ export default function Users({ data }) {
         labelLine={false}
       >
         {data.map((_, index) => (
-          <Cell key={`cell-${index}`} fill={shuffled[index % colors.length]} />
+          <Cell key={`cell-${index}`} style={{ outline: "none" }} fill={shuffled[index % colors.length]} />
         ))}
       </Pie>
       <Tooltip content={CustomTooltip} />
