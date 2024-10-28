25e92488337c740e1bb53e64feae574aacd07658
Gary Tou <gary@garytou.com>
2024-10-27 22:44:54 -0700

    Update usages

--- app/models/wire.rb
+++ app/models/wire.rb
@@ -217,8 +217,8 @@ class Wire < ApplicationRecord
   end
 
   validate on: :create do
-    if !user.admin? && usd_amount_cents < (Event.find(event.id).minimumn_wire_amount_cents)
-      errors.add(:amount, " must be more than or equal to #{ApplicationController.helpers.render_money event.minimumn_wire_amount_cents} (USD).")
+    if !user.admin? && usd_amount_cents < (Event.find(event.id).plan.minimum_wire_amount_cents)
+      errors.add(:amount, " must be more than or equal to #{ApplicationController.helpers.render_money event.plan.minimum_wire_amount_cents} (USD).")
     end
   end
 
