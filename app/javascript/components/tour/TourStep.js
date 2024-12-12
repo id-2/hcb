e7e5c01397854f396ccaeea04f0dba77d10b7fc5
Manu G <manusvathgurudath@gmail.com>
2024-12-12 02:12:01 +0000

    Remove `.z-*` classes

--- app/javascript/components/tour/TourStep.js
+++ app/javascript/components/tour/TourStep.js
@@ -105,7 +105,7 @@ function TourStep({
 
   return (
     <div
-      className="card border b--info overflow-visible z4 max-width-1"
+      className="card border b--info overflow-visible z-[41] max-width-1"
       ref={floating}
       style={{
         position: strategy,
