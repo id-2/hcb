43fd8fead98ee949731cc87e50a43199f50c16cf
Gary Tou <gary@garytou.com>
2025-02-26 13:53:46 -0800

    Drop demo mode check in Mailer

--- app/mailers/event/onboarding_mailer.rb
+++ app/mailers/event/onboarding_mailer.rb
@@ -4,17 +4,11 @@ class Event
     before_action :set_event
     before_action :set_user
 
-    before_action :ensure_event_is_not_demo
-
     def getting_started
       mail to: @user.email_address_with_name, subject: "👋 Welcome to HCB! Let's get started"
     end
 
     private
-
-    def ensure_event_is_not_demo
-      raise AbortDeliveryError if @event.demo_mode?
-    end
 
     def set_event
       @event = params[:event]
