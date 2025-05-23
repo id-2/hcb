cbe100b0a3482daad05d67389e384899a49e5c8f
Samuel Fernandez <me@sfernandez.dev>
2025-05-23 15:40:00 +0000

    Allow readers to view receipt status image

--- app/policies/hcb_code_policy.rb
+++ app/policies/hcb_code_policy.rb
@@ -34,7 +34,7 @@ class HcbCodePolicy < ApplicationPolicy
   end
 
   def receipt_status?
-    user&.admin? || gte_member_in_events? || user_made_purchase?
+    user&.admin? || present_in_events? || user_made_purchase?
   end
 
   def pin?
