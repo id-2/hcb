b7270d5c8632e1fbd3307c45e02075bd1876f326
Sam Poder <sampoder@berkeley.edu>
2025-06-19 10:44:55 -0500

    Rewrite `ReceiptPolicy#destroy?`

--- app/policies/receipt_policy.rb
+++ app/policies/receipt_policy.rb
@@ -3,11 +3,36 @@
 class ReceiptPolicy < ApplicationPolicy
   def destroy?
     user&.admin? ||
-      (record&.receiptable&.event&.users&.include?(user) && unlocked?) ||
+      (record&.receiptable&.event &&
+        OrganizerPosition.role_at_least?(user, record.receiptable.event, :member) &&
+        unlocked?
+      ) ||
       # Checking if receiptable is nil prevents unauthorized
       # deletion when user no longer has access to an org
       (record&.receiptable.nil? && record&.user == user) ||
       (record&.receiptable.instance_of?(Reimbursement::Expense) && record&.user == user && unlocked?)
+  end
+
+  def destroy?  
+    return true if user.admin?
+    return false if record.nil?
+    
+    # any members of events should be able to modify receipts.
+    if record.receiptable.event
+      return true if OrganizerPosition.role_at_least?(user, record.receiptable.event, :member) && unlocked?
+    end
+    
+    # the receipt is in receipt bin.
+    if record.receiptable.nil?
+      return record.user == user
+    end
+    
+    # the receipt is on a reimbursement report. people making reports may not be in the organization.
+    if record.receiptable.instance_of?(Reimbursement::Expense)
+      return true if record.receiptable.report.user == user && unlocked?
+    end
+    
+    return false
   end
 
   def link?
