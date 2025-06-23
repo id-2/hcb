a5c7fdb88a4f16c7a237a0a70fb6d30d33cdd798
Manu G <manusvathgurudath@gmail.com>
2025-06-23 00:43:10 +0000

    Improve controller!

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -50,18 +50,11 @@ class InvoicesController < ApplicationController
 
     relation = relation.search_description(params[:q]) if params[:q].present?
 
-    allowed_sorts = {
-      "created_at"    => "invoices.created_at",
-      "status"        => "invoices.status",
-      "sponsors.name" => "sponsors.name",
-      "amount_due"    => "invoices.amount_due"
-    }
+    allowed_sorts = %w[created_at status sponsors.name amount_due]
     allowed_directions = %w[asc desc]
 
-    sort_column = allowed_sorts[params[:sort]] || "invoices.created_at"
+    sort_column = allowed_sorts.include?(params[:sort]) ? params[:sort] : "created_at"
     sort_direction = allowed_directions.include?(params[:direction]) ? params[:direction] : "desc"
-
-    relation = relation.joins(:sponsor) if sort_column == "sponsors.name"
     relation = relation.order("#{sort_column} #{sort_direction}")
 
     @sponsor = Sponsor.new(event: @event)
