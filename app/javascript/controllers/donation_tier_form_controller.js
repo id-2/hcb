9d6316def60b7f79b5bffebd2f0b3381b2ed1fb1
Manu G <manusvathgurudath@gmail.com>
2025-07-02 20:40:08 +0000

    Implement donation tier form controller with debounce save functionality

---
+++ app/javascript/controllers/donation_tier_form_controller.js
@@ -0,0 +1,23 @@
+import { Controller } from '@hotwired/stimulus';
+
+export default class extends Controller {
+  static targets = ["field", "checkbox", "loader"];
+
+  connect() {
+    this.fieldTargets.forEach((field) => {
+      field.addEventListener('change', this.saveWithDebounce.bind(this));
+    });
+
+    this.checkboxTarget.addEventListener('change', () => this.loaderTarget.classList.remove('hidden'));
+  }
+
+  saveWithDebounce(event) {
+    clearTimeout(this.saveTimeout);
+    this.saveTimeout = setTimeout(() => {
+      const form = event.target.closest('form');
+      if (form) {
+        form.requestSubmit();
+      }
+    }, 500);
+  }
+}
