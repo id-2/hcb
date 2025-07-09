8db2e0364b3521f559fe3700fe3150bcba444fdc
Manu G <manusvathgurudath@gmail.com>
2025-07-09 20:40:08 +0000

    Add published toggle to donation tiers and update existing records

--- db/migrate/20250702221525_add_publish_toggle_to_donation_tier.rb
+++ db/migrate/20250702221525_add_publish_toggle_to_donation_tier.rb
@@ -1,5 +1,8 @@
 class AddPublishToggleToDonationTier < ActiveRecord::Migration[7.2]
   def change
     add_column :donation_tiers, :published, :boolean, default: false, null: false
+
+    # Set all existing donation tiers to published
+    Donation::Tier.update_all(published: true)
   end
 end
