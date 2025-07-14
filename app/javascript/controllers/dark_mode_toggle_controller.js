cc2d9c1301ed069eb1ab6538ba9a5bff391efb92
Manu G <manusvathgurudath@gmail.com>
2025-07-14 20:17:42 +0000

    Remove blog embed update from theme check initialization

--- app/javascript/controllers/dark_mode_toggle_controller.js
+++ app/javascript/controllers/dark_mode_toggle_controller.js
@@ -12,7 +12,7 @@ export default class extends Controller {
 
   updateActiveCheck() {
     const selectedTheme = getCookie('theme') || 'system'
-    this.updateBlogEmbed(selectedTheme)
+    // this.updateBlogEmbed(selectedTheme)
     this.toggleTargets.forEach(target => {
       const check = target.querySelector('svg')
       const targetTheme = target.getAttribute('data-value')
