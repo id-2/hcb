9f98b16e8e08100e59ed324c3858d00e21a9f089
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2025-07-14 12:47:12 -0400

    Update app/policies/event_policy.rb
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- app/policies/event_policy.rb
+++ app/policies/event_policy.rb
@@ -75,6 +75,7 @@ class EventPolicy < ApplicationPolicy
 
   def announcement_overview?
     return false if !@event.is_public && @all_announcements.empty? && !organizer_signed_in?
+
     true
   end
 
