0fcd63a6a144608524f7fa8c79b130ff9d7a9616
Samuel Fernandez <me@sfernandez.dev>
2025-07-21 14:31:07 -0400

    Discard changes to .devcontainer/boot.sh

--- .devcontainer/boot.sh
+++ .devcontainer/boot.sh
@@ -3,3 +3,8 @@ yarn install
 yarn build
 
 sudo chown -R vscode:vscode /usr/local/bundle
+
+# Setup PostgreSQL
+bundle exec rails db:prepare
+bundle exec rails db:test:prepare
+bundle exec rails db:migrate
